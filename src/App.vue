1becd0256191bac40fc318bebbebddf3b6c1e2da
Artyom Guryanov <guryanov.artyom@gmail.com>
2018-06-26 12:03:29 +0600

    some style enhancements

--- src/App.vue
+++ src/App.vue
@@ -16,5 +16,55 @@ export default {
 </script>
 
 <style>
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
+  }
 
+  body {
+    margin: 0;
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  p {
+    margin-top: 0;
+  }
+
+  .container {
+    max-width: 1225px;
+    padding-left: 15px;
+    padding-right: 15px;
+    margin-left: auto;
+    margin-right: auto;
+  }
+
+  .row {
+    display: flex;
+    flex-wrap: wrap;
+    margin-left: -15px;
+    margin-right: -15px;
+  }
+
+  .col {
+    padding-left: 15px;
+    padding-right: 15px;
+  }
+
+  .col-sm-4 {
+    width: 33.3333%;
+  }
+
+  .col-sm-9 {
+    width: 75%;
+  }
+
+  .col-sm-3 {
+    width: 25%;
+  }
 </style>
--- src/components/Products.vue
+++ src/components/Products.vue
@@ -4,7 +4,7 @@
       <div class="row">
         <div class="col col-sm-9">
           <div class="products__top">
-            <div class="products__quantity" v-if="view === 'list'">Total number of products: {{ numberOfProducts }}</div>
+            <div class="products__quantity">Total number of products: {{ numberOfProducts }}</div>
             <div class="view-toggles">
               <label class="view-toggles__title">Change view</label>
               <ul class="view-toggles__list">
@@ -166,56 +166,8 @@
 </script>
 
 <style>
-  *,
-  *::before,
-  *::after {
-    box-sizing: border-box;
-  }
-
-  body {
-    margin: 0;
-  }
-
-  h1,
-  h2,
-  h3,
-  h4,
-  h5,
-  h6,
-  p {
-    margin-top: 0;
-  }
-
-  .container {
-    max-width: 1225px;
-    padding-left: 15px;
-    padding-right: 15px;
-    margin-left: auto;
-    margin-right: auto;
-  }
-
-  .row {
-    display: flex;
-    flex-wrap: wrap;
-    margin-left: -15px;
-    margin-right: -15px;
-  }
-
-  .col {
-    padding-left: 15px;
-    padding-right: 15px;
-  }
-
-  .col-sm-4 {
-    width: 33.3333%;
-  }
-
-  .col-sm-9 {
-    width: 75%;
-  }
-
-  .col-sm-3 {
-    width: 25%;
+  .products {
+    padding: 50px 0;
   }
 
   .view-toggles__list {
